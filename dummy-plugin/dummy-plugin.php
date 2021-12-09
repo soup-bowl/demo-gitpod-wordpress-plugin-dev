dba60b37cf80afc116f999758cb6f0b020022dcc
soup-bowl <code@soupbowl.io>
2021-12-09 22:45:40 +0000

    Kickstart the GitPod demo.

---
+++ dummy-plugin/dummy-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Dummy Plugin
+Description: Like the Hello Dolly plugin, but Bee Movie instead.
+Author: soup-bowl, WordPress Contributors
+Version: master
+Author URI: https://www.soupbowl.io
+*/
+
+echo "hello world";die();
